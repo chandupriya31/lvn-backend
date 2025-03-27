7b7eeeffd036d68eb142147c0eb6a41b2d991093
Rakesh <rakeshdevarakonda2000@gmail.com>
2025-03-27 17:51:57 +0530

    organization

--- app/models/opportunity.model.js
+++ app/models/opportunity.model.js
@@ -1,70 +1,63 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose from "mongoose";
 
-const opportunitySchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    required: true,
-  },
-  time: {
-    type: String,
-    required: true,
-  }
-,  
-  numberOfVolunteers: {
-    type: Number,
-    required: true,
-    min: 1,
-  },
-  organization: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    //later remove this User ..and ref it matches
-
-    required: true,
-  },
-
-  volunteerApplications: [
-    {
-      volunteer: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User", 
-    //later remove this User ..and ref it matches
-
-      },
-      applicationStatus: {
-        type: String,
-        enum: ["Pending", "Accepted", "Rejected"],
-        default: "Pending",
-      },
-      appliedAt: {
-        type: Date,
-        default: Date.now,
-      },
+const opportunitySchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
     },
-  ],
-
-  location: {
-    type: String,
-    required: true,
+    description: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+    numberOfVolunteers: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+    organization: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // Change "User" later if needed
+      required: true,
+    },
+    volunteerApplications: [
+      {
+        volunteer: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User", // Change "User" later if needed
+          required: true,
+        },
+        applicationStatus: {
+          type: String,
+          enum: ["Pending", "Accepted", "Rejected"],
+          default: "Pending",
+        },
+        appliedAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    location: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["Posted", "Closed", "Completed", "Pending"],
+      default: "Posted",
+    },
   },
-  status: {
-    type: String,
-    enum: ["Posted", "Closed", "Completed","Pending"],
-    default: "Posted",
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 
 export const Opportunity = mongoose.model("Opportunity", opportunitySchema);
