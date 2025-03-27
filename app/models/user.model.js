19772360cd4b9f979a418a8c28852b94baa1fe65
chandupriya <priyadavuluru@gmail.com>
2025-03-27 11:14:07 +0530

    added user schema

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,1 +1,15 @@
 import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+    name: String,
+    email: String,
+    password: String,
+    avatar: String,
+    otp: Number,
+    is_verified: {
+        type: Boolean,
+        default: false
+    },
+}, { timestamps: true })
+
+export const User = model('User', userSchema)
